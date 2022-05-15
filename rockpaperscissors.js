1202dc42eaadf53047d657cff94fe31590844354
gozooit <gozoo.it@protonmail.com>
2022-05-15 17:32:06 +0200

    rpsOneRound first try

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -14,3 +14,31 @@ function computerPlay() {
     }
     return res;
 }
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function rpsOneRound(playerSelection = capitalize(input("What do you chose ? (Rock, Paper or Scissors ?)\n")), computerSelection=computerPlay()) {
+    let win;
+    if (playerSelection === computerSelection) {
+        return `${playerSelection} vs ${computerSelection}, it's even !\n`;
+    }
+    if (playerSelection === "Rock") {
+        win = (computerSelection === "Scissors") || false;
+    } else if (playerSelection === "Paper") {
+        win = (computerSelection === "Rock") || false;
+    } else if (playerSelection === "Scissors") {
+        win = (computerSelection === "Paper") || false;
+    } else {
+        return "Please enter one of the following : Rock, Paper or Scissors·\n";
+    }
+
+    if (win) {
+        return `You Won! ${playerSelection} beats ${computerSelection}\n`
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}\n`
+    }
+}
+
+rpsOneRound();
