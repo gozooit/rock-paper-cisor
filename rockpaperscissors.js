01bea04ea15849c4790bcc74d0cca90898f68d20
gozooit <gozoo.it@protonmail.com>
2022-05-19 21:35:43 +0200

    can play a game by clicking buttons

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -56,52 +56,73 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function isWinner(result) {
+// Duplicate to create log later
+// function isWinner(result) {
+//     if (typeof(result) === "string") {
+//         if (result === "even") {
+//             console.log(`${playerSelection} vs ${computerSelection}, it's even !\n`);
+//             console.log(`Round ${i + 1} - Current score is -> You : ${playerScore} // Computer : ${computerScore}\n`);
+//         } else if (result === "error") {
+//             console.log(`Round ${i + 1} - ERROR! Please enter one of the following : Rock, Paper or Scissors·\n`);
+//         }
+//     }
+//     else if (typeof(result) === "boolean") {
+//         if (result) {
+//             playerScore++;
+//             console.log(`You Won! ${playerSelection} beats ${computerSelection}\n`);
+//             console.log(`Round ${i + 1} - Current score is -> You : ${playerScore} // Computer : ${computerScore}\n`);
+//         } else {
+//             computerScore++;
+//             console.log(`You Lose! ${computerSelection} beats ${playerSelection}\n`);
+//             console.log(`Round ${i + 1} - Current score is -> You : ${playerScore} // Computer : ${computerScore}\n`);
+//         }
+//     }
+// }
+
+
+function isWinner(result, playerSelection, computerSelection) {
     if (typeof(result) === "string") {
         if (result === "even") {
             console.log(`${playerSelection} vs ${computerSelection}, it's even !\n`);
-            console.log(`Round ${i + 1} - Current score is -> You : ${playerScore} // Computer : ${computerScore}\n`);
         } else if (result === "error") {
-            console.log(`Round ${i + 1} - ERROR! Please enter one of the following : Rock, Paper or Scissors·\n`);
+            console.log(`ERROR! Please enter one of the following : Rock, Paper or Scissors·\n`);
         }
     }
     else if (typeof(result) === "boolean") {
         if (result) {
-            playerScore++;
             console.log(`You Won! ${playerSelection} beats ${computerSelection}\n`);
-            console.log(`Round ${i + 1} - Current score is -> You : ${playerScore} // Computer : ${computerScore}\n`);
         } else {
-            computerScore++;
             console.log(`You Lose! ${computerSelection} beats ${playerSelection}\n`);
-            console.log(`Round ${i + 1} - Current score is -> You : ${playerScore} // Computer : ${computerScore}\n`);
         }
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i<5; i++) {
-        playerSelection = prompt("What do you chose ? (Rock, Paper or Scissors ?)\n").toLowerCase();
-        computerSelection = computerPlay();
-        win = playRound(playerSelection, computerSelection);
-
-    }
-    if (playerScore > computerScore) {
-        console.log(`Congratulations! You won against the computer : ${playerScore} - ${computerScore}`);
-    } else if (computerScore > playerScore) {
-        console.log(`What a shame! You lost against the computer : ${computerScore} - ${playerScore}`);
-    } else if (computerScore === playerScore) {
-        console.log(`Close one! It's even : ${playerScore} - ${computerScore}`)
-    }
-}
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for (let i = 0; i<5; i++) {
+//         playerSelection = prompt("What do you chose ? (Rock, Paper or Scissors ?)\n").toLowerCase();
+//         computerSelection = computerPlay();
+//         win = playRound(playerSelection, computerSelection);
+//         isWinner ici mais osef
+//     }
+//     if (playerScore > computerScore) {
+//         console.log(`Congratulations! You won against the computer : ${playerScore} - ${computerScore}`);
+//     } else if (computerScore > playerScore) {
+//         console.log(`What a shame! You lost against the computer : ${computerScore} - ${playerScore}`);
+//     } else if (computerScore === playerScore) {
+//         console.log(`Close one! It's even : ${playerScore} - ${computerScore}`)
+//     }
+// }
 
 const buttons = document.querySelectorAll('button');
 
 function logTest(e) {
-    console.log(this.classList.value);
-    result = playRound(this.classList.value, computerPlay());
-    isWinner(result);
+    // console.log(this.classList.value);
+    let playerSelection = this.classList.value;
+    computerSelection = computerPlay();
+    result = playRound(playerSelection, computerSelection);
+    isWinner(result, playerSelection, computerSelection);
 };
 
 buttons.forEach(button => button.addEventListener('click', logTest));
